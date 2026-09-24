3aa4450d582bfac9ebf2b9fa604d2fb183c2362c
redfoxtrystman <158617274+redfoxtrystman@users.noreply.github.com>
2026-09-23 22:30:26 -0700

    PKVault V8 alpha7: restore normal National Dex item routing

--- pkvault-rebuild/v8/pokedex-item-v8.tsx
+++ pkvault-rebuild/v8/pokedex-item-v8.tsx
@@ -2,45 +2,32 @@ import React from "react";
 import { withErrorCatcher } from "../../error/with-error-catcher";
 import { Route } from "../../routes/pokedex";
 import { UIPokedexItem } from '../../ui/pokedex/pokedex-item/ui-pokedex-item';
-import type { DexProfile } from "./hooks/use-pokedex-items";
 
 export type PokedexItemProps = {
   species: number;
   speciesName: string;
   isSeen: boolean;
-  dexProfile?: DexProfile;
   children: React.ReactNode[];
 };
 
-export const PokedexItem: React.FC<PokedexItemProps> = withErrorCatcher("item", React.memo(({
-  species,
-  speciesName,
-  isSeen,
-  dexProfile,
-  children,
-}) => {
+export const PokedexItem: React.FC<PokedexItemProps> = withErrorCatcher("item", React.memo(({ species, speciesName, isSeen, children }) => {
   const navigate = Route.useNavigate();
 
-  const selected = Route.useSearch({
-    select: (search) => search.selected === species && search.dexProfile === dexProfile,
-  });
+  const selected = Route.useSearch({ select: (search) => search.selected === species });
 
   const onClick = React.useMemo(() => isSeen
     ? () =>
       navigate({
-        search: search => ({
-          ...search,
+        search: {
           selected: selected ? undefined : species,
-          dexProfile: selected ? undefined : dexProfile,
-          selectedSaveId: undefined,
-        }),
+        },
       })
     : undefined,
-    [ navigate, isSeen, selected, species, dexProfile ],
+    [ navigate, isSeen, selected, species ],
   );
 
   return <UIPokedexItem
-    id={`species-${dexProfile ?? 'national'}-${species}`}
+    id={`species-${species}`}
     species={species}
     label={speciesName}
     selected={selected}
